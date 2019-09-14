8973f7f1507c2c05d057de3fecfa492e61471b0a
Eloise Barrow <eloisebarrow@Eloises-Air.home>
2019-09-14 13:19:32 -0400

    add json raw data file

---
+++ data.js
@@ -0,0 +1,100 @@
+const eloiseRestaurantData = [
+  {
+    "name": "Lil Frankies",
+    "borough": "Manhattan",
+    "address": "19 1st Avenue",
+    "website": "https://www.lilfrankies.com/",
+    "description": "Casual Italian - great atmosphere and food, don't miss the truffle pizza.",
+    "latitude": "40.7236056",
+    "longitude": "-73.9906031"
+  },
+  {
+    "name": "Death & Co",
+    "borough": "Manhattan",
+    "address": "433 East 6th Street",
+    "website": "https://www.deathandcompany.com/location/death-and-company-new-york/",
+    "description": "Speakeasy that opened when speakeasys were cool - still serves one of the best martinis in the city.",
+    "latitude": "40.72592",
+    "longitude": "-73.9868392"
+  },
+  {
+    "name": "The Grill",
+    "borough": "Manhattan",
+    "address": "99 East 52nd Street",
+    "website": "https://thegrillnewyork.com/",
+    "description": "Upscale dining - the pasta a la presse is la best.",
+    "latitude": "40.7583177,",
+    "longitude": "-73.9740621"
+  },
+  {
+    "name": "Corner Bistro",
+    "borough": "Manhattan",
+    "address": "331 West 4th Street",
+    "website": "https://cornerbistrony.com/",
+    "description": "Old school bar that serves local beer McSorleys and one of the cheapest (good) burgers around.",
+    "latitude": "40.7379852",
+    "longitude": "-74.0059257"
+  },
+  {
+    "name": "Minca",
+    "borough": "Manhattan",
+    "address": "536 East 5th Street",
+    "website": "http://newyorkramen.com/minca/",
+    "description": "Super casual, delicious ramen.",
+    "latitude": "40.723998",
+    "longitude": "-73.9851431"
+  }
+];
+
+const eloiseSitesData = [
+  {
+    "name": "Greenwood Cemetery",
+    "borough": "Brooklyn",
+    "address": "500 25th Street",
+    "website": "https://www.green-wood.com/",
+    "description": "One of the oldest cemetaries in the 5 boroughs, beautiful and quiet.",
+    "latitude": "40.6580779",
+    "longitude": "-73.9970434"
+  },
+  {
+    "name": "Coney Island",
+    "borough": "Brooklyn",
+    "address": "1208 Surf Avenue",
+    "website": "https://www.coneyisland.com/about-coney-island-usa",
+    "description": "Ride the cyclone and grab a hot dog from Nathan's (attractions are seasonal).",
+    "latitude": "40.5753293",
+    "longitude": "-73.9819771"
+  },
+  {
+    "name": "Rubin Museum of Art",
+    "borough": "Manhattan",
+    "address": "150 West 17th Street",
+    "website": "https://rubinmuseum.org/",
+    "description": "Unique exhibitions centered on Himalayan art.",
+    "latitude": "40.7401773",
+    "longitude": "-74.0000082"
+  },
+  {
+    "name": "The Met Cloisters",
+    "borough": "Manhattan",
+    "address": "99 Margaret Corbin Drive, Fort Tryon Park",
+    "website": "https://www.metmuseum.org/visit/plan-your-visit/met-cloisters",
+    "description": "Beautiful offshoot of The Met, make sure to check out the park for great views of the Hudson river.",
+    "latitude": "40.8648628",
+    "longitude": "-73.9339215"
+  },
+  {
+    "name": "Blue Note Jazz Club",
+    "borough": "Manhattan",
+    "address": "131 West 3rd Street",
+    "website": "http://www.bluenotejazz.com/newyork/index.shtml",
+    "description": "Old school jazz club that still hosts some of the greatest old and current musicians.",
+    "latitude": "40.7309083",
+    "longitude": "-74.0028498"
+  }
+];
+
+module.exports = {
+  eloiseRestaurantData,
+  eloiseSitesData
+}
