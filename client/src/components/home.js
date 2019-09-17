057997d53b0e0551664d14d8d6fccb93074b4201
jerni81 <jerni81@yahoo.com>
2019-09-17 16:57:47 -0400

    Links active/destinations grid styled

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -1,9 +1,19 @@
 import React from "react";
+import { Link } from 'react-router-dom'
 
 function Home() {
   return (
     <div>
-      <h1>Home</h1>
+      <Link to="/bites">
+        <h3>BITES</h3>
+      </Link>
+      <Link to="/sites">
+        <h3>SITES</h3>
+      </Link>
+      <Link to='/favorites/user/:user_id'>
+        <h3>FAVORITES</h3>
+      </Link>
+
     </div>
   )
 }
