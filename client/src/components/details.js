b161be2cd67ead443041fe4c28eb3ce5eb62c8b5
Jad <jadizzedin@gmail.com>
2019-09-20 09:48:10 -0400

    added images and styling to details pages

--- client/src/components/details.js
+++ client/src/components/details.js
@@ -6,19 +6,29 @@ function Details(props) {
 
   const ratingObj = props.ratings.find((rating) => rating.destinationId === props.data.id);
 
+
+  const backgroundStyles = {
+    backgroundImage: `url(${props.data.image_url})`,
+    backgroundSize: 'cover',
+    width: '100%',
+    height: '80%'
+  }
+
   return (
-    <div className="details">
-      <h1>{props.data.name}</h1>
-      <p>{props.data.borough}</p>
-      <p>{props.data.description}</p>
-      <p>{props.data.address}</p>
-      <a href={props.data.website} target="_blank" rel="noopener noreferrer">Website</a>
-      <Rating
-        destinationId={props.data.id}
-        onStarClick={props.onStarClick}
-        rating={ratingObj ? ratingObj.rating : 0}
-        className="rating-div"
-      />
+    <div style={backgroundStyles} className="details">
+      <div className="info-container">
+        <h1>{props.data.name}</h1>
+        <p>{props.data.borough}</p>
+        <p>{props.data.description}</p>
+        <p>{props.data.address}</p>
+        <a href={props.data.website} target="_blank" rel="noopener noreferrer">Visit Website</a>
+        <Rating
+          destinationId={props.data.id}
+          onStarClick={props.onStarClick}
+          rating={ratingObj ? ratingObj.rating : 0}
+          className="rating-div"
+        />
+      </div>
     </div>
   )
 }
